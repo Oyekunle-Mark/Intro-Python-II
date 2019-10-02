490d51817968a32015da662d8bab9dd65a7e999b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-02 15:27:24 +0100

    Added items to the rooms on instatiation

--- src/adv.py
+++ src/adv.py
@@ -6,21 +6,21 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", ["Lantern", "Axe", "Bellow"]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ["Coal", "Needle", "Gun"]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ["Pellet", "Plates", "Book"]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ["Shovel", "Barrow", "Anvil"]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", ["Chest", "Bag", "Spear"]),
 }
 
 
@@ -112,4 +112,4 @@ while True:
             break
 
     elif len(user_input) == 2:
-        print('yay, two verbs!')
+        command, item = user_input
