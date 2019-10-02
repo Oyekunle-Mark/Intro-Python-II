4ce757f5ccbce1de9ad2b72173e191a1fab953a0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-02 14:39:26 +0100

    Support to add items to the room

--- src/room.py
+++ src/room.py
@@ -7,10 +7,14 @@ class Room:
     s_to = None
     e_to = None
     w_to = None
+    items = []
 
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def add_item(self, item):
+        self.items.append(item)
 
     def __str__(self):
         return f"In room {self.name}.\n{self.description}"
