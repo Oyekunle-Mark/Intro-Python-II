8c4fda92e31a1cb041c1c7c3c1cd412f445c5455
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-02 13:51:09 +0100

    Added docstring

--- src/player.py
+++ src/player.py
@@ -6,3 +6,9 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+
+    def __str__(self):
+        return f"Player {self.name} is in room {self.current_room}"
+
+    def __repr__(self):
+        return f"{self.name}, {self.current_room}"
