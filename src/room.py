d9f6b2d3a5c819dae13fbf9895680ea1596cbdc0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-02 13:51:32 +0100

    The room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    n_to = None
+    s_to = None
+    e_to = None
+    w_to = None
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"In room {self.name}.\n{self.description}"
+
+    def __repr__(self):
+        return f"{self.name}, {self.description}"
