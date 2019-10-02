3d11e9d19905b737e4fc79d9cc1f2c2ee6266e85
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-02 14:48:34 +0100

    Support for a player to add item

--- src/player.py
+++ src/player.py
@@ -3,9 +3,14 @@
 
 
 class Player:
+    items = []
+
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+
+    def add_item(self, item):
+        self.items.append(item)
 
     def __str__(self):
         return f"Player {self.name} is in room {self.current_room}"
